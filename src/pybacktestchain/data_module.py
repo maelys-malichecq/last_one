87d4ee8856df6912e0a036e218d7adac3c8bb906
Mehmet-Bsg <basagacm@outlook.fr>
2024-10-05 12:47:15 +0200

    implement changes strategy

--- src/pybacktestchain/data_module.py
+++ src/pybacktestchain/data_module.py
@@ -101,10 +101,64 @@ class Information:
         return data
 
     def compute_information(self, t : datetime):  
-        pass
+        # Get the data module 
+        data = self.slice_data(t)
+        # the information set will be a dictionary with the data
+        information_set = {}
 
-    def compute_portfolio(self, t : datetime,  information_set : dict):
-        pass
+        # sort data by ticker and date
+        data = data.sort_values(by=[self.company_column, self.time_column])
+
+        # expected return per company
+        data['return'] =  data.groupby(self.company_column)[self.adj_close_column].pct_change().mean()
+        data['kurtosis'] = data.groupby(self.company_column)['return'].transform(lambda x: x.kurt())
+        # expected return by company 
+        information_set['expected_return'] = data.groupby(self.company_column)['return'].mean().to_numpy()
+        information_set['kurtosis'] = data.groupby(self.company_column)['kurtosis'].first().to_numpy()
+        # covariance matrix
+
+        # 1. pivot the data
+        data = data.pivot(index=self.time_column, columns=self.company_column, values=self.adj_close_column)
+        # drop missing values
+        data = data.dropna(axis=0)
+        # 2. compute the covariance matrix
+        covariance_matrix = data.cov()
+        # convert to numpy matrix 
+        covariance_matrix = covariance_matrix.to_numpy()
+        # add to the information set
+        information_set['covariance_matrix'] = covariance_matrix
+        information_set['companies'] = data.columns.to_numpy()
+        return information_set
+
+    def compute_portfolio_bis(self, t : datetime,  information_set : dict):
+        mu = information_set['expected_return']
+        Sigma = information_set['covariance_matrix']
+        kurtosis = information_set['kurtosis']
+
+        gamma = 1 # risk aversion parameter
+        delta = 0.1  # kurtosis aversion parameter
+        n = len(mu)
+        
+        # objective function
+        obj = lambda x: x.dot(Sigma).dot(x) + gamma/2 * x.dot(Sigma).dot(x) + delta * np.sum(kurtosis * x)
+        # constraints
+        cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+        # bounds, allow short selling, +- inf 
+        bounds = [(0, 1)] * n
+        # initial guess, equal weights
+        x0 = np.ones(n) / n
+        # minimize
+        res = minimize(obj, x0, constraints=cons, bounds=bounds)
+
+        # prepare dictionary 
+        portfolio = {k: None for k in information_set['companies']}
+
+        # if converged update
+        if res.success:
+            for i, company in enumerate(information_set['companies']):
+                portfolio[company] = res.x[i]
+        
+        return portfolio
        
         
 @dataclass
@@ -148,10 +202,10 @@ class FirstTwoMoments(Information):
 
         # expected return per company
         data['return'] =  data.groupby(self.company_column)[self.adj_close_column].pct_change().mean()
-        
+    
         # expected return by company 
         information_set['expected_return'] = data.groupby(self.company_column)['return'].mean().to_numpy()
-
+        
         # covariance matrix
 
         # 1. pivot the data
---
+++ src/pybacktestchain/pybacktestchain.py
@@ -0,0 +1,171 @@
+
+import yfinance as yf
+import pandas as pd 
+from sec_cik_mapper import StockMapper
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+import logging 
+from scipy.optimize import minimize
+import numpy as np
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+#---------------------------------------------------------
+# Constants
+#---------------------------------------------------------
+
+UNIVERSE_SEC = list(StockMapper().ticker_to_cik.keys())
+
+#---------------------------------------------------------
+# Functions
+#---------------------------------------------------------
+
+# function that retrieves historical data on prices for a given stock
+def get_stock_data(ticker, start_date, end_date):
+    """get_stock_data retrieves historical data on prices for a given stock
+
+    Args:
+        ticker (str): The stock ticker
+        start_date (str): Start date in the format 'YYYY-MM-DD'
+        end_date (str): End date in the format 'YYYY-MM-DD'
+
+    Returns:
+        pd.DataFrame: A pandas dataframe with the historical data
+
+    Example:
+        df = get_stock_data('AAPL', '2000-01-01', '2020-12-31')
+    """
+    stock = yf.Ticker(ticker)
+    data = stock.history(start=start_date, end=end_date, auto_adjust=False, actions=False)
+    # as dataframe 
+    df = pd.DataFrame(data)
+    df['ticker'] = ticker
+    df.reset_index(inplace=True)
+    return df
+
+def get_stocks_data(tickers, start_date, end_date):
+    """get_stocks_data retrieves historical data on prices for a list of stocks
+
+    Args:
+        tickers (list): List of stock tickers
+        start_date (str): Start date in the format 'YYYY-MM-DD'
+        end_date (str): End date in the format 'YYYY-MM-DD'
+
+    Returns:
+        pd.DataFrame: A pandas dataframe with the historical data
+
+    Example:
+        df = get_stocks_data(['AAPL', 'MSFT'], '2000-01-01', '2020-12-31')
+    """
+    # get the data for each stock
+    # try/except to avoid errors when a stock is not found
+    dfs = []
+    for ticker in tickers:
+        try:
+            df = get_stock_data(ticker, start_date, end_date)
+            # append if not empty
+            if not df.empty:
+                dfs.append(df)
+        except:
+            logging.warning(f"Stock {ticker} not found")
+    # concatenate all dataframes
+    data = pd.concat(dfs)
+    return data
+
+#---------------------------------------------------------
+# Classes 
+#---------------------------------------------------------
+
+# Class that represents the data used in the backtest. 
+@dataclass
+class DataModule:
+    data: pd.DataFrame
+
+# Interface for the information set 
+@dataclass
+class Information:
+    s: timedelta # Time step (rolling window)
+    data_module: DataModule # Data module
+    time_column: str = 'Date'
+    company_column: str = 'ticker'
+    adj_close_column: str = 'Close'
+
+    def slice_data(self, t : datetime):
+         # Get the data module 
+        data = self.data_module.data
+        # Get the time step 
+        s = self.s
+        # Get the data only between t-s and t
+        data = data[(data[self.time_column] >= t - s) & (data[self.time_column] < t)]
+        return data
+
+    def compute_information(self, t : datetime):  
+        # Get the data module 
+        data = self.slice_data(t)
+        # the information set will be a dictionary with the data
+        information_set = {}
+
+        # sort data by ticker and date
+        data = data.sort_values(by=[self.company_column, self.time_column])
+
+        # expected return per company
+        data['return'] =  data.groupby(self.company_column)[self.adj_close_column].pct_change().mean()
+        # kurtosis on data set
+        data['kurtosis'] = data.groupby(self.company_column)['return'].transform(lambda x: x.kurt())
+
+        # expected return by company on information set
+        information_set['expected_return'] = data.groupby(self.company_column)['return'].mean().to_numpy()
+        # kurtosis on information set
+        information_set['kurtosis'] = data.groupby(self.company_column)['kurtosis'].first().to_numpy()
+        # covariance matrix
+
+        # 1. pivot the data
+        data = data.pivot(index=self.time_column, columns=self.company_column, values=self.adj_close_column)
+        # drop missing values
+        data = data.dropna(axis=0)
+        # 2. compute the covariance matrix
+        covariance_matrix = data.cov()
+        # convert to numpy matrix 
+        covariance_matrix = covariance_matrix.to_numpy()
+        # add to the information set
+        information_set['covariance_matrix'] = covariance_matrix
+        information_set['companies'] = data.columns.to_numpy()
+        return information_set
+
+    def compute_portfolio_bis(self, t : datetime,  information_set : dict):
+        mu = information_set['expected_return']
+        Sigma = information_set['covariance_matrix']
+        kurtosis = information_set['kurtosis']
+
+        gamma = 1 # risk aversion parameter
+        delta = 0.1  # kurtosis aversion parameter
+        n = len(mu)
+        
+        # objective function
+        obj = lambda x: x.dot(Sigma).dot(x) + gamma/2 * x.dot(Sigma).dot(x) + delta * np.sum(kurtosis * x)
+        # constraints
+        cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+        # bounds, only allow long positions
+        bounds = [(0, 1)] * n
+        # initial guess, equal weights
+        x0 = np.ones(n) / n
+        # minimize
+        res = minimize(obj, x0, constraints=cons, bounds=bounds)
+
+        # prepare dictionary 
+        portfolio = {k: None for k in information_set['companies']}
+
+        # if converged update
+        if res.success:
+            for i, company in enumerate(information_set['companies']):
+                portfolio[company] = res.x[i]
+        
+        return portfolio
+
+
+
+
+
+
+
