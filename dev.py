bc2c74586324f3d46d174c54f088a33e8e8bac22
EnzoVCode <enzo.volpato09@gmail.com>
2024-10-06 14:54:20 +0200

    Variance_only computing function added

--- dev.py
+++ dev.py
@@ -1,11 +1,11 @@
 #%%
-from pybacktestchain.data_module import get_stocks_data, UNIVERSE_SEC, DataModule, Information, FirstTwoMoments
+from pybacktestchain.data_module import get_stocks_data, UNIVERSE_SEC, DataModule, Information, FirstTwoMoments, Variance_optimizing
 from datetime import timedelta
 
 # pick 10 random stocks
 import random
 random.seed(42)
-stocks = random.sample(UNIVERSE_SEC, 10)
+stocks = random.sample(UNIVERSE_SEC, 20)
 
 df = get_stocks_data(stocks, '2000-01-01', '2020-12-31')
 
@@ -18,9 +18,20 @@ info = FirstTwoMoments(s = timedelta(days=360),
                        time_column='Date',
                        company_column='ticker',
                        adj_close_column='Adj Close')
+
+# Create the FirstTwoMoments object
+info_1 = Variance_optimizing(s = timedelta(days=360), 
+                       data_module = data_module,
+                       time_column='Date',
+                       company_column='ticker',
+                       adj_close_column='Adj Close')
 # %%
 t = df['Date'].max()
 information_set = info.compute_information(t)
 portfolio = info.compute_portfolio(t, information_set)
 
+# %%
+information_set = info_1.compute_information(t)
+portfolio = info_1.compute_portfolio(t, information_set)
+portfolio
 # %%
